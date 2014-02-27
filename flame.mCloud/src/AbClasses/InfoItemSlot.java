6ebd1815d76a0a724e2ac58f1b334b16c2f85217
FinalF <flameyufeng@gmail.com>
2014-02-27 17:45:27 -0500

    HTTP download packageanalysis finished

---
+++ flame.mCloud/src/AbClasses/InfoItemSlot.java
@@ -0,0 +1,56 @@
+package AbClasses;
+
+import Interfaces.InfoItem;
+
+public class InfoItemSlot implements InfoItem{
+	//	private String type=null;
+	//	private String size=null;
+	//	private String count=null;
+	private String[] item = new String[3];
+	
+	public InfoItemSlot(){
+		item[0]=null;
+		item[1]=null;
+		item[2]=null;
+	}
+	
+	
+	public InfoItemSlot(String type, int size, int count){
+		item[0]=type;
+		item[1]=String.valueOf(size);
+		item[2]=String.valueOf(count);
+	}
+	
+	
+	
+	 public void countPlus(){
+	 	int count = Integer.parseInt(item[2]);
+	 	count++;
+	 	item[2]=String.valueOf(count);
+	 }
+	
+	 public void sizeUpdate(String size){
+		 item[1]=size;
+	 }
+	 
+	 public void typeUpdate(String type){
+		 item[0]=String.valueOf(type);
+	 }
+	
+	 public String returnType(){
+		return item[0];
+	 }
+
+	 public int returnCount(){
+		 return Integer.parseInt(item[2]);
+	 }
+
+	 public int returnSize(){
+		 return Integer.parseInt(item[1]);
+	 }
+	 
+	 public String toString(){
+		 return (item[0]+","+item[1]+","+item[2]);
+	 }
+
+}
