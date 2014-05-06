f89d219343db5f727dbe71f70b6ee74568755c9a
FinalF <flameyufeng@gmail.com>
2014-05-05 22:12:57 -0400

    introduce the signal tuning parameter

--- MATLAB/simulation/InfoMatrix.m
+++ MATLAB/simulation/InfoMatrix.m
@@ -3,14 +3,15 @@
 %2 x_i: the coordinate
 %3. y_i: the coordinate
 %4. d_i: the distance to center (d_i is less than R)
-%5.a_i: the signal parameter, random in [1,d_i]
+%5.a_i: the signal parameter, random in [1,d_i^s]
 %6. s_i: signal strength =a_i/d_i^2
 %----------------------------------------
 
 %The geo area 1000x1000
+s=1.8; %signal tuning parameter
 R=1000;
 n=200;
-capacity=10; %maximum number of devices a group leader can support
+capacity=5; %maximum number of devices a group leader can support
 k=n/capacity;
 infoMatrix=zeros(6,n);
 
@@ -21,7 +22,7 @@ infoMatrix(1,:)=2*pi*rand(1,n);
 infoMatrix(4,:)=R/15+14*R/15*rand(1,n);
 infoMatrix(2,:)=infoMatrix(4,:).*cos(infoMatrix(1,:));
 infoMatrix(3,:)=infoMatrix(4,:).*sin(infoMatrix(1,:));
-infoMatrix(5,:)=(infoMatrix(4,:).^2-1).*rand(1,n)+1;
+infoMatrix(5,:)=(infoMatrix(4,:).^s-1).*rand(1,n)+1;
 infoMatrix(6,:)=infoMatrix(5,:)./infoMatrix(4,:).^2;
 
 
@@ -34,7 +35,7 @@ plot(infoMatrix(2,i),infoMatrix(3,i),'o','MarkerSize',3,'MarkerFaceColor','auto'
 hold on
 end
 
-%step2: sort the matrix, the last k nodes are group leader
+%step2: sort the matrix, the last k nodes are group leader,mark them out
 [r, c] = size(infoMatrix); 
 tmp=infoMatrix;
 tmp(1,:) = infoMatrix(r,:); 
@@ -47,3 +48,6 @@ for i=n-k+1:n
 plot(sortMatrix(2,i),sortMatrix(3,i),'+r','MarkerSize',3)
 hold on
 end
+
+%step3: calculate the stats: how much can we save? Assume that each device
+%has an identical unit load.
